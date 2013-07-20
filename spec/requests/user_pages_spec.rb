07b491252b5cac74474b893c40b2e841e11d2792
Diego Mendes <diego.mendes@ua.pt>
2013-07-20 12:02:57 +0100

    Add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "User pages" do
 
   subject { page }
-  
+
   describe "index" do
 
     let(:user) { FactoryGirl.create(:user) }
@@ -29,7 +29,7 @@ describe "User pages" do
         end
       end
     end
-    
+
     describe "delete links" do
 
       it { should_not have_link('delete') }
@@ -47,15 +47,23 @@ describe "User pages" do
         end
         it { should_not have_link('delete', href: user_path(admin)) }
       end
-    end  
+    end
   end
-  
-  describe "profile page" do
-  let(:user) { FactoryGirl.create(:user) }
-  before { visit user_path(user) }
 
-  it { should have_selector('h1',    text: user.name) }
-  it { should have_selector('title', text: user.name) }
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+    before { visit user_path(user) }
+
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
+  
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
   end
 
   describe "signup page" do
@@ -64,7 +72,7 @@ describe "User pages" do
     it { should have_selector('h1',    text: 'Sign up') }
     it { should have_selector('title', text: full_title('Sign up')) }
   end
-  
+
   describe "signup" do
 
     before { visit signup_path }
@@ -88,7 +96,7 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
-      
+
       describe "after saving the user" do
         before { click_button submit }
         let(:user) { User.find_by_email('user@example.com') }
@@ -99,16 +107,16 @@ describe "User pages" do
       end
     end
   end
-  
+
   describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit edit_user_path(user) }
-    
+
     before do
       sign_in user
       visit edit_user_path(user)
     end
-    
+
     describe "page" do
       it { should have_selector('h1',    text: "Update your profile") }
       it { should have_selector('title', text: "Edit user") }
@@ -120,7 +128,7 @@ describe "User pages" do
 
       it { should have_content('error') }
     end
-    
+
     describe "with valid information" do
       let(:new_name)  { "New Name" }
       let(:new_email) { "new@example.com" }
